56b85b2bb7ff14a9beb48b66a8f03d2981516836
nikhil20005 <nikhilsuryawanshi95039@gmail.com>
2025-09-28 16:07:52 +0530

    insertion sorrt

--- basics.java
+++ basics.java
@@ -17,16 +17,17 @@ public class basics {
             arr[i] = sc.nextInt();
         }
 
-        // Bubble Sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        // Insertion Sort
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+            arr[j + 1] = key;
         }
 
         // Print sorted array
