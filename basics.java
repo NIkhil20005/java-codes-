782c58c21030a70c3fdf33e2e8181497f0698a39
nikhil20005 <nikhilsuryawanshi95039@gmail.com>
2025-09-29 23:58:49 +0530

    recursion

--- basics.java
+++ basics.java
@@ -1,39 +1,16 @@
 import java.util.Scanner;
 
 public class basics {
+    public static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNumbers(n - 1);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // Take size of array
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
-
-        // Create array
-        int arr[] = new int[n];
-
-        // Take array elements
-        System.out.println("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        // Insertion Sort
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            // Move elements greater than key to one position ahead
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-
-        // Print sorted array
-        System.out.println("Sorted array:");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        int n = 5;
+        printNumbers(n);
     }
 }
